fba50f45751b28cce21e92c7090f97eb7f36cbb1
Walter Doekes <walter+github@wjd.nu>
2020-11-19 15:47:57 +0100

    gitlab-ce: Add include/adjust rules for GitLab Omnibus install

---
+++ gitlab-ce.py
@@ -0,0 +1,39 @@
+# gitlab Omnibus
+import re
+
+
+ADJUST_REGEXES = (
+    '^ruby /opt/gitlab/embedded/service/gitaly-ruby/bin/gitaly-ruby [0-9]+ /tmp/gitaly-ruby[0-9]+/socket',
+)
+ADJUST_CREGEXES = tuple(re.compile(i) for i in ADJUST_REGEXES)
+
+EXCLUDE_REGEXES = (
+    '^/opt/gitlab/embedded/bin/git --git-dir [^ ]+ cat-file --batch(-check)?$',
+)
+EXCLUDE_CREGEXES = tuple(re.compile(i) for i in EXCLUDE_REGEXES)
+
+class ProcessFormatterMixin(object):
+    def adjust(self, process):
+        super(ProcessFormatterMixin, self).adjust(process)
+
+        for idx, regex in enumerate(ADJUST_CREGEXES):
+            s = process.cmdline
+            m = regex.search(s)
+            if m:
+                process.cmdline = s[:m.start()] + ADJUST_REGEXES[idx] + s[m.end():]
+                break
+
+    def include(self, process):
+        ret = super(ProcessFormatterMixin, self).include(process)
+        if not ret:
+            return False
+
+        if process.has_parent(include_self=False, cmdline__startswith=(
+                '/opt/gitlab/embedded/bin/postgres -D /var/opt/gitlab/postgresql/data')):
+            return False
+
+        for idx, regex in enumerate(EXCLUDE_CREGEXES):
+            if regex.search(process.cmdline):
+                return False
+
+        return True

c22dfc517b9bbb036a6dfceb38955f64dbc38d53
Walter Doekes <walter+github@wjd.nu>
2022-11-09 15:24:20 +0100

    gnupg: Ignore the gpg-agent too

--- gnupg.py
+++ gnupg.py
@@ -1,10 +1,12 @@
-# dirmngr: Ignore gnupg dirmngr
+# gnupg: Ignore gnupg gpg-agent+dirmngr
 
 class ProcessFormatterMixin(object):
     def include(self, process):
         ret = super(ProcessFormatterMixin, self).include(process)
 
-        if ret and process.cmdline.startswith('dirmngr --daemon --homedir '):
+        if ret and process.cmdline.startswith((
+                'dirmngr --daemon --homedir ',
+                'gpg-agent --homedir ')):
             return False
 
         return ret

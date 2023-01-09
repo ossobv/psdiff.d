ba769fd5bf72e0ad6e0c1b8863bbd6eef3e74ce4
Walter Doekes <walter+github@wjd.nu>
2023-01-09 09:15:00 +0100

    Add lxc-only-pid1-children.py

---
+++ lxc-only-pid1-children.py
@@ -0,0 +1,16 @@
+# Filter processes that are not indirect children of pid1.
+# This is useful in LXC containers where main processes are children of
+# (its) pid1, but externally spawned processes (through lxc-attach)
+# have a different parent. We'll hide those latter processes.
+
+class ProcessFormatterMixin(object):
+    def grandparent(self, process):
+        if process.parent is None or process.parent.pid == 0:
+            return process.pid
+        return self.grandparent(process.parent)
+
+    def include(self, process):
+        if self.grandparent(process) != 1:
+            return False
+
+        return super(ProcessFormatterMixin, self).include(process)
